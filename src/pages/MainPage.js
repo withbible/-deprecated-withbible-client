10ad5d9ec05f7fff22057f32907baca4ac56efbd
Brave Kim <ygflove@bible.ac.kr>
2022-12-05 02:40:15 +0900

    :construction: 메인페이지 헤더와 바디 구분하는 디자인 적용중

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,21 +1,40 @@
-import React from "react";
-import { Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Typography, Avatar } from "@mui/material";
 
 //INTERNAL IMPORT
+import Style from "./MainPage.module.css";
 import Category from "../components/Category/Category";
 import SearchBar from "../components/SearchBar/SearchBar";
 
 const MainPage = (_) => {
+  const [userID, setUserID] = useState("");
+
+  useEffect(() => {
+    // TODO: 실제 인증 API 적용
+    const timer = setTimeout(() => setUserID("yongki150"), 1000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <>
-      <Typography>
-        Hey yongki150, what subject you want to improve today?
-      </Typography>
+    <div className={Style.pageContainer}>
+      <Avatar
+        src={userID && `https://avatars.dicebear.com/api/micah/${userID}.svg`}
+      />
 
-      <SearchBar />
+      {userID ? (
+        <Typography>{userID}님</Typography>
+      ) : (
+        <Typography>환영합니다</Typography>
+      )}
 
-      <Category />
-    </>
+      <Typography>성경졸업고사 패스를 기원합니다!</Typography>
+
+      <div className={Style.bodyContainer}>
+        <SearchBar className={Style.searchBarComponent}/>
+
+        <Category />
+      </div>
+    </div>
   );
 };
 
