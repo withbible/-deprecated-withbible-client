e78568512d3ba466d521094127cf02471cda760d
Brave Kim <ygflove@bible.ac.kr>
2022-12-06 17:10:32 +0900

    :construction: 퀴즈페이지 질문이동 버튼컨트롤 진행중

--- src/pages/QuizPage.js
+++ src/pages/QuizPage.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { Typography } from "@mui/material";
+import React, { useState, useEffect, useCallback } from "react";
+import { Typography, Stepper, Step, StepButton, Button } from "@mui/material";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
@@ -14,6 +14,8 @@ const QuizPage = () => {
     questionSeqArray: [],
     optionHistoryArray: [],
   });
+  const [activeStep, setActiveStep] = useState(0);
+  const [userOption, setUserOption] = useState({});
 
   const fetchQuestionSeqUri = `/quiz/chapter/questions?categorySeq=${categorySeq}&chapterSeq=${chapterSeq}`;
   const fetchOptionHistoryUri = `/history/chapter/user-option?categorySeq=${categorySeq}&chapterSeq=${chapterSeq}&userSeq=1`;
@@ -23,27 +25,71 @@ const QuizPage = () => {
       axios.get(fetchQuestionSeqUri),
       axios.get(fetchOptionHistoryUri),
     ]);
+
     setData({
       questionSeqArray: questionSeqResult.data.result,
       optionHistoryArray: optionHistoryResult.data.result,
     });
   };
 
-  useEffect(() => {
-    fetchQuiz();
-  }, []);
+  const setUserOptionKeys = () => {
+    for (const each of data.questionSeqArray) {
+      setUserOption((prevState) => {
+        return {...prevState, [each["question_seq"]]: null };
+      });
+    }
+  };
 
+  const memoizeQuizPageState = useCallback(() => {
+    fetchQuiz();
+    setUserOptionKeys();
+  }, [categorySeq, chapterSeq]);
+
+  useEffect(() => {
+    memoizeQuizPageState();
+  }, []);  
+
+  // ERROR HANDLE
   if (!data.questionSeqArray.length || !data.optionHistoryArray.length) {
     return <h3>데이터를 불러오는 중입니다...</h3>;
   }
 
+  const handleBack = () => {
+    setActiveStep((prevState) => prevState - 1);
+  };
+
+  const handleNext = () => {
+    setActiveStep((prevState) => prevState + 1);
+  };
+
+  const handleStep = (step) => {
+    setActiveStep(step);
+  };
+
   return (
     <Wrapper>
       카테고리: {categorySeq} (ch. {chapterSeq})
+      <Stepper nonLinear activeStep={activeStep}>
+        {data.questionSeqArray.map((each, index) => {
+          return (
+            <Step key={index} className={Style.step}>
+              <StepButton onClick={() => handleStep(index)} />
+            </Step>
+          );
+        })}
+      </Stepper>
       <Wrapper.Body>
         <div className={Style.questionContainer}>
           <img className={Style.illust} src={illust} />
           <Typography>{data.questionSeqArray[1]["question"]}</Typography>
+        </div>
+
+        <div className={Style.buttonContainer}>
+          <Button disabled={activeStep === 0} onClick={() => handleBack}>
+            Back
+          </Button>
+
+          <Button onClick={() => handleNext}>Next</Button>
         </div>
       </Wrapper.Body>
     </Wrapper>
