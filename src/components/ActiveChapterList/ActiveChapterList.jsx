978f5cf1e2cccf1d1c953902454883a54b4b7754
Brave Kim <ygflove@bible.ac.kr>
2022-12-14 05:08:25 +0900

    :lipstick: 리뷰목록 페이지 맞힌갯수 UI

--- src/components/ActiveChapterList/ActiveChapterList.jsx
+++ src/components/ActiveChapterList/ActiveChapterList.jsx
@@ -4,27 +4,44 @@ import {
   ListItem,
   ListItemButton,
   ListItemText,
+  Divider,
+  Chip,
 } from "@mui/material";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 
 //INTERNAL IMPORT
+import Style from "./ActiveChapterList.module.css";
 import { REVIEW_PAGE_PATH } from "../../constants/route";
 
-const ActiveChapterList = ({ iteratee, category, categorySeq }) => {  
+const ActiveChapterList = ({ iteratee, category, categorySeq }) => {
   return (
     <>
       <ListSubheader>{category}</ListSubheader>
 
       {iteratee.map((each, index) => {
-        const queryParameter = `?categorySeq=${categorySeq}&chapterNum=${each}`;
+        const chapterNum = each["chapter_num"];
+        const queryParameter = `?categorySeq=${categorySeq}&chapterNum=${chapterNum}`;
 
         return (
-          <ListItem key={index}>
-            <ListItemButton href={`${REVIEW_PAGE_PATH}${queryParameter}`}>
-              <ListItemText primary={`${category} ch.${each}`} />
-              <KeyboardArrowRightIcon />
-            </ListItemButton>
-          </ListItem>
+          <div key={index}>
+            <ListItem>
+              <ListItemButton
+                href={`${REVIEW_PAGE_PATH}${queryParameter}`}
+                className={Style.listItemButton}
+              >
+                <ListItemText primary={`Ch.${chapterNum}`} />
+                <KeyboardArrowRightIcon className={Style.listItemIcon} />
+                <Chip
+                  label={`
+                  맞힌갯수 ${each["hit_question_count"]}/${each["question_count"]}
+                `}
+                  size="small"
+                  className={Style.listItemChip}
+                />
+              </ListItemButton>
+            </ListItem>
+            <Divider variant="middle" />
+          </div>
         );
       })}
     </>
