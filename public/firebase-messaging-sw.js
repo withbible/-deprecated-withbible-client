f08d34065465cae8624c3d25c30e219545ed5eec
Brave Kim <ygflove@bible.ac.kr>
2023-01-12 02:23:12 +0900

    :white_check_mark: FCM 설정 및 API 연동

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FCM_API_KEY,
+  authDomain: `${process.env.REACT_APP_FCM_PROJECT_ID}.firebaseapp.com`,
+  projectId: process.env.REACT_APP_FCM_PROJECT_ID,
+  storageBucket: `${process.env.REACT_APP_FCM_PROJECT_ID}.appspot.com`,
+  messagingSenderId: process.env.REACT_APP_FCM_SENDER_ID,
+  appId: process.env.REACT_APP_FCM_APP_ID,
+  measurementId: process.env.REACT_APP_FCM_MEASUREMENT_ID,
+};
+
+initializeApp(firebaseConfig);
