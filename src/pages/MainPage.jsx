5568d3622cf6dd3e4f3cd6563a5b43c4917825bc
Brave Kim <ygflove@bible.ac.kr>
2023-03-01 21:15:21 +0900

    :lipstick: Google Adsense 적용

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useRef } from "react";
 
 // INTERNAL IMPORT
+import Style from "./page.module.css";
 import { AppBar, Category, SearchBar, Wrapper } from "../components";
 import { ChapterContext } from "../contexts/ChapterContext";
 
@@ -21,6 +22,10 @@ function MainPage() {
     fetchActiveChapter();
   }, []);
 
+  useEffect(() => {
+    (window.adsbygoogle = window.adsbygoogle || []).push({});
+  }, []);
+
   return (
     <Wrapper>
       <Wrapper.Header>
@@ -28,6 +33,12 @@ function MainPage() {
       </Wrapper.Header>
 
       <Wrapper.Body>
+        <ins
+          className={`adsbygoogle ${Style.ads}`}
+          data-ad-client="ca-pub-1919598055512436"
+          data-ad-slot="1678485541"
+        />
+
         <SearchBar />
         <Category />
       </Wrapper.Body>
